cd53003486123a92e0c2ce9013d6f6abe496c2db
xurshidbekweb <xurshid.ermaxamatov@gmail.com>
2021-11-29 22:18:12 +0500

    it's product project

---
+++ app.js
@@ -0,0 +1,13 @@
+let leftBlock = document.getElementById('hidden')
+let bars = document.querySelector('fa')
+// let bars = addEventListener('click', function (){
+//     leftBlock.style.left = '150px'
+//     leftBlock.style.transition = '3s'
+//     leftBlock.style.display = 'inline-block'
+// })
+function click(){
+    leftBlock.style.left = '140px'
+    leftBlock.style.transition = '3s'
+    // leftBlock.style.display = 'inline-block'
+    leftBlock.style.opacity = '1'
+}
